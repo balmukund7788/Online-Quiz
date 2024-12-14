11eefc89ec98ded3a38e3397af0f81e58192bc63
Balmukund Chaudhary <balmukundchaudhary7788@gmail.com>
2024-12-14 13:53:36 +0530

    Add files via upload
    Developed an online quiz website with interactive features to create, manage, and take quizzes. Focused on user-friendly design, robust functionality, and seamless performance across devices.

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "A. HyperText Markup Language",
+        options: [
+            "A. HyperText Markup Language",
+            "B. HighText Machine Language",
+            "C. Hyperloop Machine Language",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "B. Cascading Style Sheets",
+        options: [
+            "A Computer Style Sheets",
+            "B. Cascading Style Sheets",
+            "C. Colorful Style Sheets",
+            "D. Creative Style Sheets"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does JSON stand for?",
+        answer: "A. JavaScript Object Notation",
+        options: [
+            "A. JavaScript Object Notation",
+            "B. Java Syntax Over Network",
+            "C. Java Source Output Name",
+            "D. JavaScript Oriented Nodes"
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which CSS property controls text size",
+        answer: "C. font-size",
+        options: [
+            "A. font-style",
+            "B. text-size",
+            "C. font-size",
+            "D. text-style"
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which is not a JavaScript data type?",
+        answer: "C. Element",
+        options: [
+            "A. Number",
+            "B. String",
+            "C. Element",
+            "D. Boolean"
+
+        ]
+    }
+];
